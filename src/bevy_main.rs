62590f116d587db9c69472369c1820f6f6e0e783
Neko The Catgirl <theanicoline03@gmail.com>
2025-04-13 13:49:24 +0200

    Basic Window Creation

--- src/bevy_main.rs
+++ src/bevy_main.rs
@@ -1,1 +1,51 @@
 //! Beginning of the bevy initialization
+
+use bevy::{app::{App, AppExit, PluginGroup}, window::{Window, WindowPlugin}, DefaultPlugins};
+
+struct ConfiguredDefaultPlugins;
+
+impl PluginGroup for ConfiguredDefaultPlugins {
+    fn build(self) -> bevy::app::PluginGroupBuilder {
+        let backend = {
+            
+            #[cfg(any(target_os = "windows", target_os = "linux"))]
+            {
+                Some(bevy::render::settings::Backends::VULKAN)
+            }
+
+            #[cfg(target_os = "macos")]
+            {
+                Some(bevy::render::settings::Backends::METAL)
+            }
+
+            // Optional: fallback if none match (useful if you're targeting more OSes)
+            #[cfg(not(any(target_os = "windows", target_os = "macos", target_os = "linux")))]
+            {
+                None
+            }
+        };
+
+        DefaultPlugins
+            .build()
+            .set(bevy::render::RenderPlugin {
+                render_creation: bevy::render::settings::RenderCreation::Automatic(bevy::render::settings::WgpuSettings {
+                    backends: backend,
+                    ..Default::default()
+                }),
+                synchronous_pipeline_compilation: false,
+            })
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Rust Station 13".to_owned(),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            })
+    }
+}
+
+pub fn start_client() -> AppExit {
+    App::new()
+        .add_plugins(ConfiguredDefaultPlugins)
+        .run()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::{thread::sleep, time::Duration};
+use std::{process::exit, thread::sleep, time::Duration};
 
 use args::Arguments;
 use clap::Parser;
-use tracing::{debug, Level};
+use tracing::{debug, info, Level};
 
 mod bevy_main;
 mod args;
 
+#[allow(unused)]
 fn main() {
     let args = Arguments::parse();
     
@@ -23,4 +24,44 @@ fn main() {
         sleep(Duration::from_secs(5));
     }
     debug!("Proceeding despite warning, prepare for large log files.");
+
+    let (command, port, ip, max_players, gui) = match args.command {
+        args::MainCommand::Client => {
+            info!("Starting up client!");
+            let status = bevy_main::start_client();
+            let status = match status {
+                bevy::app::AppExit::Success => "Success".into(),
+                bevy::app::AppExit::Error(non_zero) => format!("Error Code {non_zero}"),
+            };
+            debug!("App exited with {status}");
+            exit(0); // Exit early to prevent the need to return the values
+        },
+        args::MainCommand::Server { command, port, ip, max_players, gui } => {
+            debug!("Server selected, continue execution");
+            (command, port, ip, max_players, gui)
+        },
+    };
+
+    match command {
+        args::ServerCommands::Create { name, description } => {
+            info!("Preparing server creation");
+            // Check if the server already exists...
+        },
+        args::ServerCommands::Start { server } => {
+            info!("Looking for server...");
+            // Check if the server exists...
+            info!("Found server! Starting up server");
+        },
+        args::ServerCommands::Wipe { server } => {
+            info!("Looking for server...");
+            // Check if the server exists...
+            info!("Found server! Wiping persistant data");
+        },
+        args::ServerCommands::ConfigureDB { for_server, address, port, user, password, database } => {
+            info!("Looking for server...");
+            // Check if the server exists...
+            info!("Found server! Preparing to configure database for the server.");
+        },
+    }
+
 }
